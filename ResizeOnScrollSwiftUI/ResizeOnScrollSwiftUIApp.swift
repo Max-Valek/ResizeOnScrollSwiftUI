938e51327cfdc1998e59fa36972fb354ae01a8dc
Max Valek <valek.maxwell@gmail.com>
2023-04-07 11:02:40 -0500

    Initial Commit

---
+++ ResizeOnScrollSwiftUI/ResizeOnScrollSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ResizeOnScrollSwiftUIApp.swift
+//  ResizeOnScrollSwiftUI
+//
+//  Created by Max Valek on 4/7/23.
+//
+
+import SwiftUI
+
+@main
+struct ResizeOnScrollSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
